0525909c084a1223fae808a1e01941205e09e272
harsh <pharshpathak703@gmail.com>
2025-10-04 18:37:46 +0530

    Header.tsx Created

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+// import {Button} from "@/components/ui/button";
+
+import Header from "@/components/ui/Header";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children : ReactNode}) => {
+    return (
+            <main className="min-h-screen text-gray-400">
+                <Header></Header>
+                <div className="container py-10">
+                    {children}
+                </div>
+            </main>
+    );
+};
+
+export default Layout;
